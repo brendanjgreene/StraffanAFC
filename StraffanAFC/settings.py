f41e6db18feba07a140ccc03b63bfff73f48a2b7
Brendan Greene <brendanjgreene@gmail.com>
2017-06-09 18:50:03 +0100

    templates reconstruction

--- StraffanAFC/settings.py
+++ StraffanAFC/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'StraffanAFC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
