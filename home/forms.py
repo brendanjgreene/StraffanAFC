fca3d67b9b3f4e3a70246b0d3cc9c5dc2ae844cb
Brendan Greene <brendanjgreene@gmail.com>
2017-06-25 20:10:30 +0100

    widgets added to Player form

--- home/forms.py
+++ home/forms.py
@@ -14,6 +14,10 @@ class PlayerForm(forms.ModelForm):
     class Meta:
         model = Player
         fields = ['name', 'last_name', 'parent_name', 'mobile', 'email', 'date_of_birth', 'team']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'players First Name'}),
+            'date_of_birth': forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD'}),
+        }
 
 
 class UserLoginForm(forms.Form):
