787d1a3423b186eacea7a4f1ab013a63f775c041
Brendan Greene <brendanjgreene@gmail.com>
2017-07-13 16:24:24 +0100

    separating code to get ready for deployment

---
+++ settings/dev.py
@@ -0,0 +1,12 @@
+from base import *
+
+DEBUG = True
+
+INSTALLED_APPS.append('debug_toolbar')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
---
+++ settings/staging.py
@@ -0,0 +1,13 @@
+from base import *
+
+DEBUG = False
+
+INSTALLED_APPS.append('debug_toolbar')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
