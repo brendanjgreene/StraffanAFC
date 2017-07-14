a71840587447751df4d39f97880b3e3fc44872a0
Brendan Greene <brendanjgreene@gmail.com>
2017-07-14 18:11:58 +0100

    staging database

--- settings/staging.py
+++ settings/staging.py
@@ -1,7 +1,15 @@
 from base import *
 import dj_database_url
+import settings
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 DATABASES['default'] = dj_database_url.parse(
     "mysql://bf4174401dd1d4:3eb1be20@eu-cdbr-west-01.cleardb.com/heroku_a77520ba3d93d67?reconnect=true")
