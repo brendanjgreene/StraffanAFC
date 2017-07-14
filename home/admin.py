f102a91e406fe8de5cf9b66501316bf2b2ba4391
Brendan Greene <brendanjgreene@gmail.com>
2017-07-14 18:43:21 +0100

    register profile with admin

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from models import Player, Team, StaffTitle
+from models import Player, Team, StaffTitle, Profile
 
 # Register your models here.
-admin.site.register(Player)
+admin.site.register(Player, Profile)
 admin.site.register(Team)
 admin.site.register(StaffTitle)
