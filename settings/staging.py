93fb48ffca0bf9011c27841b222e9650e1a4a165
Brendan Greene <brendanjgreene@gmail.com>
2017-07-14 20:57:16 +0100

    staging

--- settings/staging.py
+++ settings/staging.py
@@ -8,10 +8,10 @@ INSTALLED_APPS.append('storages')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'mydatabase',
     }
 }
 
 DATABASES['default'] = dj_database_url.parse(
-    "mysql://bf4174401dd1d4:3eb1be20@eu-cdbr-west-01.cleardb.com/heroku_a77520ba3d93d67?reconnect=true")
+    "mysql://bf4174401dd1d4:3eb1be20@eu-cdbr-west-01.cleardb.com/heroku_a77520ba3d93d67?")
