0338fb1c80e40d1c4b61c32db793abd3b6245dfc
Brendan Greene <brendanjgreene@gmail.com>
2017-07-16 10:57:28 +0100

    media root experiment

--- settings/base.py
+++ settings/base.py
@@ -89,7 +89,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join('straffanafc.herokuapp.com', 'media')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),  # static directory at the project level
