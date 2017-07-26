23389d253bbba80ca77878fff3ced16d2f86bdb3
Brendan Greene <brendanjgreene@gmail.com>
2017-07-26 18:07:33 +0100

    local secret

--- settings/base.py
+++ settings/base.py
@@ -4,7 +4,7 @@ from secret import *
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'LOCAL_SECRET_KEY'
+SECRET_KEY = LOCAL_SECRET_KEY
 
 
 ALLOWED_HOSTS = ['straffanafc.herokuapp.com', '127.0.0.1', 'localhost']
