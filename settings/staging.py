7e9451e8428f71020fca7014ff751dfbe8f50403
Brendan Greene <brendanjgreene@gmail.com>
2017-07-27 14:14:37 +0100

    SERVE MEDIA FILES

--- settings/staging.py
+++ settings/staging.py
@@ -14,8 +14,6 @@ MEDIA_ROOT = '/media/'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 MEDIA_URL = S3_URL + MEDIA_ROOT
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 ''' 
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
