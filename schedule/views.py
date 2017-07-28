4fe2eb3910fc07a7259fa0c3d4cac93a7629c40a
Brendan Greene <brendanjgreene@gmail.com>
2017-07-28 18:44:30 +0100

    completed web scraping of schedule

--- schedule/views.py
+++ schedule/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from lxml import html
 import requests
 from home.views import Team
 from bs4 import BeautifulSoup
-import pandas as pd
-
-
-'''def get_schedule(request):
-    return render(request, 'schedule/schedule.html', {'teams': Team.objects.all()})'''
 
 
 def get_schedule(request):
@@ -19,21 +13,27 @@ def get_schedule(request):
 
     # Turn the HTML into a Beautiful Soup object
     soup = BeautifulSoup(r.content, 'html.parser')
+    # find all active games using the class
     games = soup.find_all(class_='ui-state-default')
+    # create an empty list
     games_list = []
     for game in games:
+        # get date and league using previous sibling
         date = game.findPreviousSibling(class_='ui-widget-header').get_text()
         league = game.findPreviousSibling().get_text()
+        # find all columns in the games row and turn them into text
         row = game.find_all('td')
         home = row[0].get_text()
         away = row[3].get_text()
         venue = row[4].get_text()
         time = row[5].get_text()
+        # use all the text to use them for details in games list
         details = [home, away, venue, time, league, date]
         games_list.append(details)
 
-    args = {'games_list': games_list,
-            'teams': Team.objects.all()
+    args = {
+        'games_list': games_list,
+        'teams': Team.objects.all()
     }
     return render(request, 'schedule/schedule.html', args)
 
